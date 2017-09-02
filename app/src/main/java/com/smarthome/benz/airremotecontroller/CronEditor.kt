cbd121e8c5710646501f8e4f2fb10c3d2de6699a
BenZStory <zhengbin0320@gmail.com>
2017-09-02 20:03:42 +0800

    toggle and tint week btns

--- app/src/main/java/com/smarthome/benz/airremotecontroller/CronEditor.kt
+++ app/src/main/java/com/smarthome/benz/airremotecontroller/CronEditor.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.TextView
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
@@ -29,7 +30,9 @@ class CronEditor: AppCompatActivity(), View.OnClickListener {
     var mQueue: RequestQueue? = null
     var isAddingNew : Boolean = true
 
-    var newCron = CmdCron(0, 1, 12, 0, AirCmd(0, true))
+    var newCron = CmdCron(0, 0, 12, 0, AirCmd(0, true))
+    var weekdays_mask: Int = 31 shl 1
+    var weekends_mask: Int = 3 shl 6
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -80,8 +83,30 @@ class CronEditor: AppCompatActivity(), View.OnClickListener {
         btn_toggle.setOnClickListener(this)
         btn_work_mode.setOnClickListener(this)
         btn_wind_speed.setOnClickListener(this)
+        day_1.setOnClickListener(this)
+        day_2.setOnClickListener(this)
+        day_3.setOnClickListener(this)
+        day_4.setOnClickListener(this)
+        day_5.setOnClickListener(this)
+        day_6.setOnClickListener(this)
+        day_7.setOnClickListener(this)
+        day_option_weekday.setOnClickListener(this)
+        day_option_weekend.setOnClickListener(this)
+
+        tintAllWeekBtns()
 
         mQueue = Volley.newRequestQueue(this)
+    }
+
+    fun tintAllWeekBtns() {
+        tintWeekBtn(1, day_1)
+        tintWeekBtn(2, day_2)
+        tintWeekBtn(3, day_3)
+        tintWeekBtn(4, day_4)
+        tintWeekBtn(5, day_5)
+        tintWeekBtn(6, day_6)
+        tintWeekBtn(7, day_7)
+        tintBriefWeekOptionBtns()
     }
 
     fun send_cron() {
@@ -175,8 +200,130 @@ class CronEditor: AppCompatActivity(), View.OnClickListener {
                     false -> tv_toggle.text = "OFF"
                 }
             }
+
+            day_1 -> {
+                Log.i(TAG, "onClick day1")
+                toggleWeek(1, day_1)
+            }
+
+            day_2 -> {
+                toggleWeek(2, day_2)
+            }
+
+            day_3 -> {
+                toggleWeek(3, day_3)
+            }
+
+            day_4 -> {
+                toggleWeek(4, day_4)
+            }
+
+            day_5 -> {
+                toggleWeek(5, day_5)
+            }
+
+            day_6 -> {
+                toggleWeek(6, day_6)
+            }
+
+            day_7 -> {
+                toggleWeek(7, day_7)
+            }
+
+            day_option_weekday -> {
+                toggleBriefWeekOption(1)
+            }
+
+            day_option_weekend -> {
+                toggleBriefWeekOption(2)
+            }
         }
     }
+
+    fun tintWeekBtn(week:Int, v:TextView?) {
+        Log.i(TAG, "tintWeekBtn, week: " + week)
+        var week_flag = 1 shl week
+        if (week <= 5) {
+            if (this.newCron.day == 0 || (this.newCron.day and week_flag > 0)) {
+                v!!.setBackgroundColor(getColor(R.color.SpringGreen))
+            } else {
+                v!!.setBackgroundColor(getColor(R.color.lavender))
+            }
+        } else {
+            if (this.newCron.day == 0 || (this.newCron.day and week_flag > 0)) {
+                v!!.setBackgroundColor(getColor(R.color.Goldenrod))
+            } else {
+                v!!.setBackgroundColor(getColor(R.color.lavender))
+            }
+        }
+    }
+
+    fun tintBriefWeekOptionBtns() {
+        Log.i(TAG, "tintBriefWeekOptionBtns, day: " + this.newCron.day)
+        if (this.newCron.day == 0) {
+            day_option_weekday.setBackgroundColor(getColor(R.color.SpringGreen))
+            day_option_weekend.setBackgroundColor(getColor(R.color.Goldenrod))
+            return
+        }
+
+        val weekday_flag = this.newCron.day and weekdays_mask
+        if (weekday_flag == weekdays_mask) {
+            day_option_weekday.setBackgroundColor(getColor(R.color.SpringGreen))
+        } else if (weekday_flag > 0) {
+            day_option_weekday.setBackgroundColor(getColor(R.color.LightGray))
+        } else {
+            day_option_weekday.setBackgroundColor(getColor(R.color.lavender))
+        }
+
+        val weekend_flag = this.newCron.day and weekends_mask
+        if (weekend_flag == weekends_mask) {
+            day_option_weekend.setBackgroundColor(getColor(R.color.Goldenrod))
+        } else if (weekend_flag > 0) {
+            day_option_weekend.setBackgroundColor(getColor(R.color.LightGoldenrod))
+        } else {
+            day_option_weekend.setBackgroundColor(getColor(R.color.lavender))
+        }
+    }
+
+    fun toggleBriefWeekOption(tag: Int) {
+        if (this.newCron.day == 0) {
+            Log.i(TAG, "toggleWeek, day_flag day is 0")
+            this.newCron.day = weekdays_mask or weekends_mask
+        }
+        if (tag == 1) {
+            val weekday_flag = this.newCron.day and weekdays_mask
+            if (weekday_flag > 0) {
+                // clear weekday
+                this.newCron.day = this.newCron.day and weekdays_mask.inv()
+            } else {
+                // set weekdays_mask
+                this.newCron.day = this.newCron.day or weekdays_mask
+            }
+        } else if (tag == 2) {
+            val weekend_flag = this.newCron.day and weekends_mask
+            if (weekend_flag > 0) {
+                this.newCron.day = this.newCron.day and weekends_mask.inv()
+            } else {
+                this.newCron.day = this.newCron.day or weekends_mask
+            }
+        }
+        tintAllWeekBtns()
+    }
+
+    fun toggleWeek(week:Int, v:TextView) {
+        Log.i(TAG, "toggleWeek, week: " + week)
+        if (this.newCron.day == 0) {
+            Log.i(TAG, "toggleWeek, day_flag day is 0")
+            this.newCron.day = weekdays_mask or weekends_mask
+        }
+        Log.i(TAG, "toggleWeek, day_flag before: " + this.newCron.day)
+        var week_flag = 1 shl week
+        this.newCron.day = this.newCron.day xor week_flag;
+        Log.i(TAG, "toggleWeek, day_flag after: " + this.newCron.day)
+        tintWeekBtn(week, v)
+        tintBriefWeekOptionBtns()
+    }
+
 
     fun refreshViewParams() {
         Log.i(TAG, "refreshViewParams")
